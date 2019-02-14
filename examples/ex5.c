6f820f75eb33e9ff6928060365a25872511aba85
wondermacros <wondermacros@jiivonen-laptop>
2019-02-14 08:24:43 +0200

    Add HTML attribute api macros. Add an example.

---
+++ examples/ex5.c
@@ -0,0 +1,25 @@
+#include <web_elements/web_elements.h>
+
+int main()
+{
+    struct we* doc = htmlHTML(attrID(_("Main")), attrLANG(_("fi-FI")), attrDIR(_("LTR")), attrCLASS(_("Public")),
+        htmlHEAD(
+            htmlTITLE(_("An Example Page"))
+        ),
+        htmlBODY(
+            htmlP(_("This page was created using Web Elements.")),
+            htmlBR(),
+            htmlP(_("End of page."))
+        )
+    );
+
+    struct we_view_char_buffer* view =
+        W_NEW(we_view_char_buffer, .root = doc, .size=256);
+
+    W_CALL(view,expand)(NULL);
+    printf("%s\n", view->buffer);
+    W_CALL_VOID(view,free);
+
+    return 0;
+}
+
--- src/web_elements.h
+++ src/web_elements.h
@@ -3,6 +3,7 @@
 
 #include <web_elements/elems/all.h>
 #include <web_elements/api/html.h>
+#include <web_elements/api/html_attr.h>
 #include <web_elements/api/other_elems.h>
 
 #endif
